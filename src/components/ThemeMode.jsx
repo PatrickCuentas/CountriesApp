d5ab7847a520e09d4de5b7667c1b8592c18f1180
patrick_cuentas <patrickcuentasmariano@gmail.com>
2022-01-10 08:11:45 -0500

    Setup the theme mode for the project

---
+++ src/components/ThemeMode.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { DarkMode } from "../icons/DarkMode.jsx";
+import { LightMode } from "../icons/LightMode.jsx";
+const htmlNode = document.querySelector("html");
+
+export const ThemeMode = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toogleMode = () => {
+    setDarkMode((prev) => !prev);
+  };
+
+  const checkMode = () => {
+    darkMode
+      ? htmlNode.classList.add("dark")
+      : htmlNode.classList.remove("dark");
+  };
+
+  useEffect(() => {
+    checkMode();
+  }, [darkMode]);
+
+  return (
+    <div className="flex gap-2 items-center">
+      <button className="self-baseline" onClick={toogleMode}>
+        {darkMode ? <DarkMode /> : <LightMode />}
+      </button>
+      <span className="text-xl font-semibold dark:text-white">Dark Mode</span>
+    </div>
+  );
+};
