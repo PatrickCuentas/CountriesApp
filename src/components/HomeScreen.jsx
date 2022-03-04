a66c5408ed39b5323ef7c97b7dfa8b1dd45f5af6
patrick_cuentas <patrickcuentasmariano@gmail.com>
2022-03-04 17:50:55 -0500

    Update HomeScreen for lazy loading

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,12 +1,15 @@
-import { InputSearch } from "./InputSearch.jsx";
-import { FilterSearch } from "./FilterSearch.jsx";
-import { CountryCard } from "./CountryCard.jsx";
-import { useFetch } from "../hooks/useFetch.jsx";
-import { endpoints } from "../data/endpoints.js";
+import { InputSearch } from './InputSearch.jsx'
+import { FilterSearch } from './FilterSearch.jsx'
+import { useFetch } from '../hooks/useFetch.jsx'
+import { endpoints } from '../data/endpoints.js'
+import LoadingView from './LoadingView.jsx'
+import { Suspense, lazy } from 'react'
 
-export const HomeScreen = () => {
-  const [countriesData, setCountriesData] = useFetch(endpoints.all, []);
-  const { data: countries, loading } = countriesData;
+const CountryCard = lazy(() => import('./CountryCard.jsx'))
+
+function HomeScreen() {
+  const [countriesData, setCountriesData] = useFetch(endpoints.all, [])
+  const { data: countries, loading } = countriesData
 
   return (
     <main className="flex flex-col gap-12">
@@ -14,12 +17,19 @@ export const HomeScreen = () => {
         <InputSearch />
         <FilterSearch setCountriesData={setCountriesData} />
       </div>
+
       <section id="grid" className="mb-10 px-5 grid gap-10">
         {!loading &&
-          countries.map((country) => {
-            return <CountryCard key={country.name.common} {...country} />;
+          countries.map(country => {
+            return (
+              <Suspense fallback={<LoadingView />}>
+                <CountryCard key={country.name.common} {...country} />
+              </Suspense>
+            )
           })}
       </section>
     </main>
-  );
-};
+  )
+}
+
+export default HomeScreen
